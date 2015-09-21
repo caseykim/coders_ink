f3ae0a08d1139522e7c90e2c91f1752b457800c1
Casey Kim <k.yesac@gmail.com>
2015-09-21 15:36:24 -0400

    update users

---
+++ db/migrate/20150921183945_add_colunmns_admin_avatar_username_to_users.rb
@@ -0,0 +1,8 @@
+class AddColunmnsAdminAvatarUsernameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, default: false, null: false
+    add_column :users, :username, :string, null: false
+    add_column :users, :avatar, :string, null: false,
+      default: "http://www.besttats.com/pictures/tech-savy-back-tattoo.jpeg"
+  end
+end

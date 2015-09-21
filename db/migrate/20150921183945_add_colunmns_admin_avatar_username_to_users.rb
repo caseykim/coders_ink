cc9a24af23065bf78ae2cb130169b4982369b636
Casey Kim <k.yesac@gmail.com>
2015-09-21 16:19:10 -0400

    fixed hound errors

--- db/migrate/20150921183945_add_colunmns_admin_avatar_username_to_users.rb
+++ db/migrate/20150921183945_add_colunmns_admin_avatar_username_to_users.rb
@@ -3,6 +3,6 @@ class AddColunmnsAdminAvatarUsernameToUsers < ActiveRecord::Migration
     add_column :users, :admin, :boolean, default: false, null: false
     add_column :users, :username, :string, null: false
     add_column :users, :avatar, :string, null: false,
-      default: "http://www.besttats.com/pictures/tech-savy-back-tattoo.jpeg"
+               default: "http://www.besttats.com/pictures/tech-savy-back-tattoo.jpeg"
   end
 end

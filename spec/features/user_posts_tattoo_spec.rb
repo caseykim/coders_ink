7388ced2d31ffdd4f41b10876ef0885c0c9fa97b
Casey Kim <k.yesac@gmail.com>
2015-09-21 11:36:08 -0400

    initial user stories

---
+++ spec/features/user_posts_tattoo_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "User posts a tattoo to be reviewed", %(
+  As a user
+  I want to submit a tattoo for review
+  So that I could get feedback from others
+
+  Acceptance Criteria
+  [ ] I must provide a name for the tattoo
+  [ ] I must provide supply a url for tattoo image
+  [ ] I may optionally add a description
+  [ ] I must see an error if I submit an incorrect form
+) do
+
+  scenario "" do
+
+  end
+
+
+end
---
+++ spec/features/user_views_all_tattoos_spec.rb
@@ -0,0 +1,13 @@
+require ''
+
+feature "User views all tattoos", %(
+  As a user
+  I want to view recently posted tattoos
+  So that I could read or write reviews for tattoo
+
+  Acceptance Criteria
+  [ ] I must see the name of each tattoo posting
+  [ ] I must see the tattoos listed in order, most recently posted first
+) do
+
+end

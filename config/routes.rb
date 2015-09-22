dce2775f27fe580ff40fdba4a0c0499c9ed0ff5c
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-09-22 11:17:52 -0400

    added the ability to create tattoos

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show]
-  resources :tattoos, only: [:index, :show]
+  resources :tattoos, only: [:index, :show, :new, :create]
 end
--- spec/features/user_posts_tattoo_spec.rb
+++ spec/features/user_posts_tattoo_spec.rb
@@ -1,20 +1,66 @@
 require 'rails_helper'
 
-feature "User posts a tattoo to be reviewed", %(
-  As a user
-  I want to submit a tattoo for review
-  So that I could get feedback from others
+feature "User submits a tattoo of his own", %(
+As a user
+I want to submit a tattoo for review
+So that I could get feedback from others
 
   Acceptance Criteria
   [ ] I must provide a name for the tattoo
   [ ] I must provide supply a url for tattoo image
   [ ] I may optionally add a description
   [ ] I must see an error if I submit an incorrect form
+  [ ] I must be brought to the tattoo details page after submission
 ) do
 
-  scenario "" do
+  scenario 'visitor creates a tattoo' do
+    user = FactoryGirl.create(:user)
 
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_content('Sign Out')
+
+    visit tattoos_path
+
+    click_link "Add A Tattoo"
+
+    fill_in "Title", with: "Brand New Tattoo"
+    fill_in "Description", with: "Hurt a lot!"
+    fill_in "Image URL", with: "https://pbs.twimg.com/media/BQMYIh2CMAAmGqp.jpg"
+
+    click_button "Submit"
+
+    urlcss = "img[src*='https://pbs.twimg.com/media/BQMYIh2CMAAmGqp.jpg']"
+    expect(page).to have_content('Brand New Tattoo')
+    expect(page).to have_content('Hurt a lot!')
+    expect(page).to have_css(urlcss)
   end
 
+  scenario 'visitor messes up the form' do
+    user = FactoryGirl.create(:user)
 
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_content('Sign Out')
+
+    visit tattoos_path
+
+    click_link "Add A Tattoo"
+
+    click_button "Submit"
+    errors = "Title can't be blank, Url can't be blank"
+    expect(page).to have_content(errors)
+  end
 end

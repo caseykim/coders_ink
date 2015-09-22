9fe29f0f0ce80aef05871ce40744f7f66056b29e
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-09-22 12:34:45 -0400

    Test helper added for Login functionality

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    config.include LoginHelper, type: :feature
+    config.include LoginHelper
   end
 
   # rspec-mocks config goes here. You can use an alternate test double

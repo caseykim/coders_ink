c31dc5246fc21e416d39badacea8c6e60940555d
Trish Z <trishzag@gmail.com>
2015-09-23 14:33:33 -0400

    modified user.rb and foundation setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # mount_uploader :avatar, AvatarUploader
 
   has_many :tattoos
   has_many :reviews

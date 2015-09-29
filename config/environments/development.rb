a4033b0ee99b9ba7f8d001eb3740c80c3c43779d
SasquatchShawn <srmiranda1@gmail.com>
2015-09-29 15:18:20 -0400

    Hound changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,15 +41,15 @@ Rails.application.configure do
    # mailcatcher config
    # to view emails generated in development:
    # `gem install mailcatcher && mailcatcher`
-   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
-   config.action_mailer.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: "localhost",
     port: 1025
-   }
+  }
 
-   config.action_mailer.default_url_options = {
+  config.action_mailer.default_url_options = {
     host: "localhost",
     port: 3000
-   }
+  }
 end

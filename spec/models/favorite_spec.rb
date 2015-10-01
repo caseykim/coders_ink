d4772a2ac66267bdffa566fdf9a5f6fd08bdf043
Trish Z <trishzag@gmail.com>
2015-10-01 14:35:54 -0400

    Review changes from pull

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Favorite, type: :model do
     it { should belong_to(:tattoo) }
     it { should belong_to(:user) }
 
-    it { should validate_presence_of(:tattoo_id) }
-    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:tattoo) }
+    it { should validate_presence_of(:user) }
 
     it "should have a user assigned to it" do
       expect(favorite.user).to be_instance_of(User)
@@ -19,10 +19,6 @@ RSpec.describe Favorite, type: :model do
 
     it "should have a tattoo assigned to it" do
       expect(favorite.tattoo).to be_instance_of(Tattoo)
-    end
-
-    it "should allow you to add a tattoo as favorite only once" do
-      should validate_uniqueness_of(:user_id).scoped_to(:tattoo_id)
     end
   end
 end

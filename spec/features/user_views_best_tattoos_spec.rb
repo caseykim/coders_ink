aa086776334d81ca71d8e534fdad67faab4488ee
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-02 10:30:09 -0400

    fixed tests/hound a little

--- spec/features/user_views_best_tattoos_spec.rb
+++ spec/features/user_views_best_tattoos_spec.rb
@@ -7,7 +7,7 @@ feature "User views the best tattoos", %(
 
   Acceptance Criteria
   [x] I must see the title and rating of each tattoo posting
-  [x] I must see only the 5 best tattoos
+  [x] I must see only the 6 best tattoos
 ) do
 
   scenario 'visitor views best tattoos' do
@@ -17,6 +17,7 @@ feature "User views the best tattoos", %(
     t4 = FactoryGirl.create(:tattoo_with_reviews)
     t5 = FactoryGirl.create(:tattoo_with_reviews)
     t6 = FactoryGirl.create(:tattoo)
+    t7 = FactoryGirl.create(:tattoo_with_reviews)
 
     visit best_tattoos_path
     expect(page).to have_content("#{t1.title}: #{t1.average_rating}")
@@ -24,6 +25,7 @@ feature "User views the best tattoos", %(
     expect(page).to have_content("#{t3.title}: #{t3.average_rating}")
     expect(page).to have_content("#{t4.title}: #{t4.average_rating}")
     expect(page).to have_content("#{t5.title}: #{t5.average_rating}")
+    expect(page).to have_content("#{t7.title}: #{t7.average_rating}")
     expect(page).to_not have_content("#{t6.title}: #{t6.average_rating}")
   end
 end
